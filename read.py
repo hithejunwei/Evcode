104fba493d7a1b5f487d3e7c16a3c8b4bc241581
hithejunwei <3326572402@qq.com>
2020-12-15 12:40:26 +0800

    read

--- read.py
+++ read.py
@@ -1,11 +1,23 @@
 def read_tsp(path):
     data=[]
     for line in open(path,'r'):
-        line = line.strip('\n')
+        if not line:
+            break
         tmp=line.split()
         data.append(tmp)
-    data=data[6:]
-    return data
+    data=data[6:-1]
+    newdata=[]
+    for tmp in data:
+        triple=[]
+        for i in range(len(tmp)):
+            tmpdata=tmp[i]
+            if i ==0:
+                tmpdata=int(tmpdata)
+            else:
+                tmpdata=float(tmpdata)
+            triple.append(tmpdata)
+        newdata.append(triple)
+    return newdata
 if __name__=='__main__':
-    data=read_tsp('eil51.tsp')
+    data=read_tsp('pr2392.tsp')
     print(data)
